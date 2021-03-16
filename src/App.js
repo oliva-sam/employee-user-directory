709ef468b031aa732a6b977e27791c3f3a7caafa
Melody Monarch <oliva.sam.so@gmail.com>
2021-03-16 15:09:31 -0500

    took out placeholder text of the sort and filter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import CardHolder from "./components/CardHolder";
 function App() {
   return (
     <>
-    <h1> Emplyees</h1>
-    <p>Sort By</p>
-    <p>Filter By</p>
+    <h1> Employees</h1>
      < CardHolder />
     </>
   );
